65eca00ca029c0233d08db02a16ba6055c21de29
Kathleen Gao <kathleen.gao@berkeley.edu>
2021-06-10 00:04:45 -0400

    style

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,3 +1,7 @@
+// document.body.onload = function(){
+//     document.body.style.display = "block"
+//   };
+
 function show(elementId) { 
     document.getElementById("sdk").style.display="none";
     document.getElementById("seam").style.display="none";
@@ -6,3 +10,17 @@ function show(elementId) {
     clicked.style.display="flex";
     // clicked.style.backgroundColor="black";
 }
+
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+          // Add the animation class
+          entry.target.classList.add('animation');
+        }
+      });
+  });
+  
+  // Tell the observer which elements to track
+  observer.observe(document.querySelector('.animation'));
