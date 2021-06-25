cfc0db4eacb34f7e47bfa443e3e7ae01706c51e0
Kathleen Gao <kathleen.gao@berkeley.edu>
2021-06-24 23:22:19 -0400

    click

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,15 +1,34 @@
-// document.body.onload = function(){
-// 	document.body.style.display = "block"
-//   };
-  
-  
-function show(elementId) { 
+document.getElementById("0")
+.addEventListener("click", function() {
+//   if (this.classList.contains("active")) {
+//     this.classList.remove("active");
+//   } else this.classList.add("active");
+	var active = document.getElementsByClassName("active");
+	for (let i=0; i < active.length; i++) {
+		active[i].className = 'inactive';
+	}
+	
+	// let button = document.getElementById(buttonId);
+	this.className = 'active';
+});
+
+document.getElementById("1")
+.addEventListener("click", function() {
+	var active = document.getElementsByClassName("active");
+	for (let i=0; i < active.length; i++) {
+		active[i].className = 'inactive';
+	}
+	// let button = document.getElementById(buttonId);
+	this.className = 'active';
+});
+
+
+function show(elementId, buttonId) { 
     document.getElementById("sdk").style.display="none";
     document.getElementById("seam").style.display="none";
 
     let clicked = document.getElementById(elementId)
     clicked.style.display="flex";
-    // clicked.style.backgroundColor="black";
 }
 
 
@@ -27,23 +46,10 @@ function showCard(ID) {
 
 }
 
-// let container = document.getElementById("topiContainer");
-
-// container.onmouseover = function() {
-
-//   hideCard("topiaria");
-//   showCard("topiariaBW");
-//   // showCard("topiariaText");
-// }
-
-// container.onmouseout = function() {
-//   showCard("topiaria");
-//   hideCard("topiariaBW");
-//   // hideCard("topiariaText");
-// }
-
 function openTopi() {
   window.open("https://newart.city/show/topiaria");
 }
 document.getElementById("topiContainer").onclick = openTopi;
+
+
 
